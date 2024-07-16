ed5a453ea3bb5c2f2fe68a4a188fe6323c4bc56f
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-15 21:03:17 -0500

    Add try-catch

--- controllers/categories.js
+++ controllers/categories.js
@@ -7,8 +7,13 @@ import {
 
 export class CategoryController {
   static async getAll(req, res) {
-    const categories = await categoryModel.getAll()
-    res.json(categories)
+    try {
+      const categories = await categoryModel.getAll()
+      res.json(categories)
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Server Error' })
+    }
   }
 
   static async getById(req, res) {
@@ -28,46 +33,61 @@ export class CategoryController {
   }
 
   static async create(req, res) {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' })
-    }
-    // Crear la URL de la imagen
-    const imageUrl = `/uploads/${req.file.filename}`
-    const categoryData = { ...req.body, image: imageUrl }
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' })
+      }
+      // Crear la URL de la imagen
+      const imageUrl = `/uploads/${req.file.filename}`
+      const categoryData = { ...req.body, image: imageUrl }
 
-    // Validar los datos de la categoría
-    const result = validateCategory(categoryData)
-    if (result.error) {
-      return res.status(400).json({ error: result.error.errors })
-    }
+      // Validar los datos de la categoría
+      const result = validateCategory(categoryData)
+      if (result.error) {
+        return res.status(400).json({ error: result.error.errors })
+      }
 
-    // Crear la nueva categoría
-    const newCategory = await categoryModel.create({ input: result.data })
-    return res.json({ message: 'Category created', newCategory })
+      // Crear la nueva categoría
+      const newCategory = await categoryModel.create({ input: result.data })
+      return res.json({ message: 'Category created', newCategory })
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Server Error' })
+    }
   }
 
   static async delete(req, res) {
-    const { id } = req.params
-    const result = await categoryModel.delete({ id })
-    if (result === false) {
-      return res.status(404).json({ message: 'Category not found' })
+    try {
+      const { id } = req.params
+      const result = await categoryModel.delete({ id })
+      if (result === false) {
+        return res.status(404).json({ message: 'Category not found' })
+      }
+      return res.json({ message: 'Category deleted' })
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Server Error' })
     }
-    return res.json({ message: 'Category deleted' })
   }
 
   static async update(req, res) {
-    console.log(req.body)
-    const result = validatePartialCategory(req.body)
+    try {
+      console.log(req.body)
+      const result = validatePartialCategory(req.body)
 
-    if (!result.success) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
+      if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+      }
+      const { id } = req.params
+
+      const updateCategory = await categoryModel.update({
+        id,
+        input: result.data
+      })
+      return res.json({ message: 'Category updated', updateCategory })
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(500).json({ error: 'Server Error' })
     }
-    const { id } = req.params
-
-    const updateCategory = await categoryModel.update({
-      id,
-      input: result.data
-    })
-    return res.json({ message: 'Category updated', updateCategory })
   }
 }
