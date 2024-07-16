b9c03bb0fd5d7d58d6ef56ab6efdee48b782d5e5
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-15 21:38:14 -0500

    Product route

---
+++ routes/products.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { upload } from '../middlewares/multer.js'
+import { ProductController } from '../controllers/products.js'
+
+// Routes
+export const productsRouter = Router()
+
+productsRouter.get('/', ProductController.getAll)
+productsRouter.get('/:id', ProductController.getById)
+productsRouter.post('/', upload.single('image'), ProductController.create)
+productsRouter.delete('/:id', ProductController.delete)
+productsRouter.patch('/:id', ProductController.update)
