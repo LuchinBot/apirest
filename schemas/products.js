be2c77f7ab6dcd7385566ec707c9d1ea1813c58d
LuchinBot <Luchinbot.2018@gmail.com>
2024-07-15 21:36:13 -0500

    Product schema

---
+++ schemas/products.js
@@ -0,0 +1,37 @@
+import zod from 'zod'
+
+const productSchema = zod.object({
+  idcategories: zod.string({
+    invalid_type_error: 'Idcategorys must be a string'
+  }),
+  title: zod.string({
+    invalid_type_error: 'Title must be a string',
+    required_error: 'Title is required',
+    min_length: 255
+  }),
+  slug: zod.string({
+    invalid_type_error: 'Slug must be a string',
+    required_error: 'Slug is required'
+  }),
+  description: zod.string({
+    invalid_type_error: 'Description must be a string',
+    required_error: 'Description is required'
+  }),
+  price: zod.decimal({
+    invalid_type_error: 'Price must be a number',
+    required_error: 'Price is required'
+  }),
+  stock: zod.number({
+    invalid_type_error: 'Stock must be a number',
+    required_error: 'Stock is required'
+  }),
+  image: zod.string().optional() // Hacer que la propiedad image sea opcional
+})
+
+export function validateProduct(object) {
+  return productSchema.safeParse(object)
+}
+
+export function validatePartialProduct(input) {
+  return productSchema.partial().safeParse(input)
+}
